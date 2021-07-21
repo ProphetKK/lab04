346444704a00fe9a02f81ed8514907cf9a22365b
Krittawit <krittawit053@gmail.com>
2021-07-21 22:56:29 +0700

    added error handling
    Q4 All

---
+++ src/views/NetworkError.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="networkError">
+    <img src="@/assets/NetworkError.jpg" />
+    <h3>
+      <a href="#" @click="$router.go(-1)">Click here</a> to try again.
+    </h3>
+  </div>
+</template>
---
+++ src/views/NotFound.vue
@@ -0,0 +1,20 @@
+<template>
+  <img src="@/assets/NotFound.jpg" />
+  <div>
+    <router-link :to="{ name: 'EventList' }"
+      >Eh Eh Ehhh BIG SMOKE IT'S ME, CARL, CHILL</router-link
+    >
+    <h3>SORRY YOU PICKED THE WRONG {{ resource }}</h3>
+  </div>
+</template>
+<script>
+export default {
+  props: {
+    resource: {
+      type: String,
+      required: true,
+      default: 'PAGE'
+    }
+  }
+}
+</script>
